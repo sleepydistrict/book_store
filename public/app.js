9f4cdca3138b1e76c0fabddafa076bf1e6e32e4c
Gregory Lesky <sleepydistrict01@gmail.com>
2017-10-14 12:32:42 -0600

    update

--- public/app.js
+++ public/app.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('books')
-);
+class BeerList extends React.Component {
+  
+    constructor() {
+      super();
+      this.state = {
+        beers: []
+      };
+    }
+  
+    componentWillMount() {
+      axios.get('/api/books')
+        .then((response) => {
+          console.log(response)
+          this.setState({
+            beers: response.data
+          })
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  
+    render() {
+      let beerItems = this.state.beers.map( (beer) => {
+        return <li>{ beer.name }</li>
+      });
+      
+      return (
+        <ul>
+          { beerItems }
+        </ul>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <BeerList />,
+    document.getElementById('books')
+  );
